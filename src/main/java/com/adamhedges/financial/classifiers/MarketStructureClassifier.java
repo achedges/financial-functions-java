7403212827c4d3a4db9b9d761f39d88857a12f2c
Adam Hedges <achedges@gmail.com>
2026-01-24 15:13:26 -0600

    MarketStructureClassifier should be instantiated so that last pivot bars can be fetched

--- src/main/java/com/adamhedges/financial/classifiers/MarketStructureClassifier.java
+++ src/main/java/com/adamhedges/financial/classifiers/MarketStructureClassifier.java
@@ -2,14 +2,33 @@ package com.adamhedges.financial.classifiers;
 
 import com.adamhedges.financial.core.bars.PriceBar;
 import com.adamhedges.financial.functions.Pivots;
+import lombok.Getter;
 
 import java.util.List;
+import java.util.Optional;
 
+@Getter
 public class MarketStructureClassifier {
 
-    public static TrendClassification classifyMarketStructure(List<PriceBar> bars, double strongTrendThreshold) {
-        List<Integer> highPivots = Pivots.get(bars, (a, b) -> a.getHigh() >= b.getHigh(), (a, b) -> a.getHigh() - b.getLow());
-        List<Integer> lowPivots = Pivots.get(bars, (a, b) -> a.getLow() <= b.getLow(), (a, b) -> b.getHigh() - a.getLow());
+    private List<PriceBar> bars = null;
+    private List<Integer> highPivots = null;
+    private List<Integer> lowPivots = null;
+    private TrendClassification trendClassification;
+
+    private final double strongTrendThreshold;
+
+    public MarketStructureClassifier() {
+        strongTrendThreshold = 0.0005;
+    }
+
+    public MarketStructureClassifier(double strongTrendThreshold) {
+        this.strongTrendThreshold = strongTrendThreshold;
+    }
+
+    public TrendClassification classifyMarketStructure(List<PriceBar> bars) {
+        this.bars = bars;
+        this.highPivots = Pivots.get(bars, (a, b) -> a.getHigh() >= b.getHigh(), (a, b) -> a.getHigh() - b.getLow());
+        this.lowPivots = Pivots.get(bars, (a, b) -> a.getLow() <= b.getLow(), (a, b) -> b.getHigh() - a.getLow());
 
         if (highPivots.size() <= 1 || lowPivots.size() <= 1) {
             return TrendClassification.Flat;
@@ -19,16 +38,39 @@ public class MarketStructureClassifier {
         double diff_l = bars.get(lowPivots.getLast()).getLow() - bars.get(lowPivots.getFirst()).getLow();
 
         if (diff_h >= strongTrendThreshold && diff_l >= strongTrendThreshold) {
-            return TrendClassification.StrongUp;
+            this.trendClassification = TrendClassification.StrongUp;
         } else if (diff_h > 0.0 && diff_l > 0.0) {
-            return TrendClassification.WeakUp;
+            this.trendClassification = TrendClassification.WeakUp;
         } else if (diff_h <= -strongTrendThreshold && diff_l <= -strongTrendThreshold) {
-            return TrendClassification.StrongDown;
+            this.trendClassification = TrendClassification.StrongDown;
         } else if (diff_h < 0.0 && diff_l < 0.0) {
-            return TrendClassification.WeakDown;
+            this.trendClassification = TrendClassification.WeakDown;
         } else {
-            return TrendClassification.Flat;
+            this.trendClassification = TrendClassification.Flat;
         }
+
+        return this.trendClassification;
+    }
+
+    private Optional<PriceBar> getLastPivot(List<Integer> pivots) {
+        if (pivots == null || pivots.isEmpty()) {
+            return Optional.empty();
+        }
+
+        int lastPivot = pivots.getLast();
+        if (bars == null || lastPivot >= bars.size()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(bars.get(lastPivot));
+    }
+
+    public Optional<PriceBar> getLastHighPivot() {
+        return getLastPivot(highPivots);
+    }
+
+    public Optional<PriceBar> getLastLowPivot() {
+        return getLastPivot(lowPivots);
     }
 
 }
--- src/test/java/com/adamhedges/financial/classifiers/TestMarketStructureClassifier.java
+++ src/test/java/com/adamhedges/financial/classifiers/TestMarketStructureClassifier.java
@@ -21,11 +21,40 @@ public class TestMarketStructureClassifier {
     }
 
     @Test
+    public void TestMarketStructureClassifier_init() {
+        MarketStructureClassifier classifier = new MarketStructureClassifier();
+        Assertions.assertEquals(0.0005, classifier.getStrongTrendThreshold());
+
+        MarketStructureClassifier classifier2 = new MarketStructureClassifier(1.0);
+        Assertions.assertEquals(1.0, classifier2.getStrongTrendThreshold());
+    }
+
+    @Test
+    public void TestMarketStructureClassifier_getLastHighPivot() {
+        MarketStructureClassifier classifier = new MarketStructureClassifier();
+
+        Assertions.assertTrue(classifier.getLastHighPivot().isEmpty());
+        Assertions.assertTrue(classifier.getLastLowPivot().isEmpty());
+
+        List<PriceBar> bars = getBars();
+        // generate an ambiguous structure
+        bars.get(4).setHigh(24.0);
+        bars.get(14).setHigh(22.0);
+        bars.get(8).setLow(16.0);
+        bars.get(18).setLow(18.0);
+        classifier.classifyMarketStructure(bars);
+
+        Assertions.assertEquals(20.71, classifier.getLastHighPivot().orElse(new PriceBar("")).getHigh(), 0.01);
+        Assertions.assertEquals(18.0, classifier.getLastLowPivot().orElse(new PriceBar("")).getLow());
+    }
+
+    @Test
     public void TestMarketStructureClassifier_Flat() {
         List<PriceBar> bars = getBars();
+        MarketStructureClassifier classifier = new MarketStructureClassifier(1.0);
 
         // base case, not enough pivots identified
-        TrendClassification trend = MarketStructureClassifier.classifyMarketStructure(bars, 1.0);
+        TrendClassification trend = classifier.classifyMarketStructure(bars);
         Assertions.assertEquals(TrendClassification.Flat, trend);
 
         // generate a perfectly flat structure
@@ -33,7 +62,7 @@ public class TestMarketStructureClassifier {
         bars.get(14).setHigh(22.0);
         bars.get(8).setLow(18.0);
         bars.get(18).setLow(18.0);
-        trend = MarketStructureClassifier.classifyMarketStructure(bars, 1.0);
+        trend = classifier.classifyMarketStructure(bars);
         Assertions.assertEquals(TrendClassification.Flat, trend);
 
         // generate an ambiguous structure
@@ -41,51 +70,55 @@ public class TestMarketStructureClassifier {
         bars.get(14).setHigh(22.0);
         bars.get(8).setLow(16.0);
         bars.get(18).setLow(18.0);
-        trend = MarketStructureClassifier.classifyMarketStructure(bars, 1.0);
+        trend = classifier.classifyMarketStructure(bars);
         Assertions.assertEquals(TrendClassification.Flat, trend);
     }
 
     @Test
     public void TestMarketStructureClassifier_WeakUp() {
+        MarketStructureClassifier classifier = new MarketStructureClassifier(1.0);
         List<PriceBar> bars = getBars();
         bars.get(4).setHigh(22.0);
         bars.get(14).setHigh(22.95); // just under 1.0
         bars.get(8).setLow(18.0);
         bars.get(18).setLow(18.95); // just under 1.0
-        TrendClassification trend = MarketStructureClassifier.classifyMarketStructure(bars, 1.0);
+        TrendClassification trend = classifier.classifyMarketStructure(bars);
         Assertions.assertEquals(TrendClassification.WeakUp, trend);
     }
 
     @Test
     public void TestMarketStructureClassifier_WeakDown() {
+        MarketStructureClassifier classifier = new MarketStructureClassifier(1.0);
         List<PriceBar> bars = getBars();
         bars.get(4).setHigh(22.0);
         bars.get(14).setHigh(21.05); // just under 1.0
         bars.get(8).setLow(18.0);
         bars.get(18).setLow(17.05); // just under 1.0
-        TrendClassification trend = MarketStructureClassifier.classifyMarketStructure(bars, 1.0);
+        TrendClassification trend = classifier.classifyMarketStructure(bars);
         Assertions.assertEquals(TrendClassification.WeakDown, trend);
     }
 
     @Test
     public void TestMarketStructureClassifier_StrongUp() {
+        MarketStructureClassifier classifier = new MarketStructureClassifier(1.0);
         List<PriceBar> bars = getBars();
         bars.get(4).setHigh(22.0);
         bars.get(14).setHigh(23.05); // just over 1.0
         bars.get(8).setLow(18.0);
         bars.get(18).setLow(19.05); // just under 1.0
-        TrendClassification trend = MarketStructureClassifier.classifyMarketStructure(bars, 1.0);
+        TrendClassification trend = classifier.classifyMarketStructure(bars);
         Assertions.assertEquals(TrendClassification.StrongUp, trend);
     }
 
     @Test
     public void TestMarketStructureClassifier_StrongDown() {
+        MarketStructureClassifier classifier = new MarketStructureClassifier(1.0);
         List<PriceBar> bars = getBars();
         bars.get(4).setHigh(22.0);
         bars.get(14).setHigh(20.95); // just under 1.0
         bars.get(8).setLow(18.0);
         bars.get(18).setLow(16.95); // just under 1.0
-        TrendClassification trend = MarketStructureClassifier.classifyMarketStructure(bars, 1.0);
+        TrendClassification trend = classifier.classifyMarketStructure(bars);
         Assertions.assertEquals(TrendClassification.StrongDown, trend);
     }
 
